fb9dc15f30b6e4ce8f04b32a2c0abed20cfb9ffa
Aditya Kumar <itsadityakr27@gmail.com>
2026-02-08 11:06:32 +0530

    Fresh Start

--- code/src/utils/apiResponse.js
+++
@@ -1,8 +0,0 @@
-class apiResponse {
-    constructor(statusCode, data, message = "Success") {
-        this.statusCode = statusCode;
-        this.data = data;
-        this.message = message;
-        this.success = statusCode < 400; //https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status
-    }
-}
